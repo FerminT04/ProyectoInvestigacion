9155a07e557570b44e173f531878d674365af194
FerminT04 <fermint0401@gmail.com>
2023-09-02 14:47:44 -0600

    Form

--- Proyecto/Form1.cs
+++ Proyecto/Form1.cs
@@ -16,5 +16,10 @@ namespace Proyecto
         {
             InitializeComponent();
         }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
     }
 }
